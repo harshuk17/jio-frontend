8c3e675f7a1368c7c043dfad859cb0122548dda5
harshuk17 <harshdhiman8410@gmail.com>
2025-07-17 12:45:13 +0530

    footer is also completed

--- src/components/ui/Fotter.jsx
+++ src/components/ui/Fotter.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const headings = [
     {
         title:"JIOCINEMA",
-        links:["For You","Movies","Tv Shows",]
+        links:["For You","Movies","Tv Shows","Sports"]
     },
     {
         title:"SUPPORT",
@@ -43,11 +43,11 @@ export default function Footer() {
     return (
         <footer className="bg-[#17181a] text-gray-300">
             <div className="md:mx-auto">
-                <div className="flex flex-col md:flex-row md:justify-between md:items-start px-8 py-4 border border-2">
-                    <div className="flex flex-col md:flex-row md:items-center gap-8 md:gap-16 mt-6 border border-3">
+                <div className="flex flex-col md:flex-row md:justify-between md:items-start px-8 py-4 ">
+                    <div className="flex flex-col md:flex-row md:items-center gap-8 md:gap-16 mt-4 ml-5 ">
                         {headings.map((heading, index) => (
-                            <div key={index} className="flex flex-col justify-items-start gap-4 boder border-2 pt-5">
-                                <h3 className="text-lg font-bold uppercase">{heading.title}</h3>
+                            <div key={index} className="flex flex-col justify-items-start gap-4 pt-5">
+                                <h3 className="text-lg font-bold uppercase text-pink-500">{heading.title}</h3>
                                 <div className="flex flex-col">
                                     {heading.links.map((link, linkIndex) => (
                                         <Link href="#" key={linkIndex}>
@@ -59,7 +59,7 @@ export default function Footer() {
                         ))}
                     </div>
                     <div className="flex flex-col gap-4 md:gap-8 mt-8">
-                        <h3 className="text-lg font-bold uppercase">Connect With Us</h3>
+                        <h3 className="text-lg font-bold uppercase text-pink-500">Connect With Us</h3>
                         <div className="flex gap-4">
                             {connectWithUsLink.map((link, linkIndex) => (
                                 <Link
@@ -78,27 +78,30 @@ export default function Footer() {
                             ))}
                         </div>
                     </div>
-                    <div className="flex flex-col gap-0 md:gap-1  md:mt-0">
-                        <h3 className="text-lg font-bold uppercase mt-8">
+                    <div className="flex flex-col gap-0 md:gap-5  md:mt-0 md:mr-10">
+                        <h3 className="text-lg font-bold uppercase mt-8 text-pink-500   ">
                             Download the App
                         </h3>
-                        <div className="flex gap-4">
-                            {appDownloadLink.map((link, linkIndex) => (
-                                <Link key={linkIndex} href="#" className="flex items-center">
-                                    <Image
-                                        src={link.icon}
-                                        width={96}
-                                        height={64}
-                                        alt={link.alt}
-                                        className="w-[96px] h-[64px] md:w-[120px] md:h-[120px]"
-                                    />
-                                </Link>
-                            ))}
-                        </div>
+                    <div className="flex gap-4 ">
+                        {appDownloadLink.map((link, linkIndex) => (
+                    <Link key={linkIndex} href="#" className="flex items-center">
+                        <div className="w-[50px] h-[50px] md:w-[70px] md:h-[70px] flex items-center justify-center bg-white rounded-lg overflow-hidden">
+                        <Image
+                        src={link.icon}
+                        alt={link.alt}
+                        width={120}
+                        height={120}
+                        className="w-20 h-20 object-cover"
+                        />
+                    </div>
+                    </Link>
+  ))}
+</div>
+
                     </div>
                 </div>
 
-                <div className="md:text-center text-start mt-8 flex w-full justify-between items-center bg-[#202123] py-4 px-4">
+                <div className=" relative md:text-center text-start mt-8 flex w-full justify-center items-center bg-[#202123] py-4 px-4">
                     <p className="md:text-sm text-xs">
                         Copyright © 2024 Viacom18 Media PVT LTD. All rights reserved.
                     </p>
@@ -107,7 +110,7 @@ export default function Footer() {
                         alt="Jio Logo"
                         width={48}
                         height={48}
-                        className="md:h-12 md:w-12 h-10 w-10"
+                        className="md:h-12 md:w-12 h-10 w-10 absolute right-4  "
                     />
                 </div>
             </div>
